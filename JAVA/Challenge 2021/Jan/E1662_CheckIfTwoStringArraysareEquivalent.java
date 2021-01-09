786831b2f2db52950fef0cb39eff120a796f42a2
Jeehwan Park <yaho2k@gmail.com>
2021-01-09 11:35:10 +0900

    Remove needless import

--- JAVA/Challenge 2021/Jan/E1662_CheckIfTwoStringArraysareEquivalent.java
+++ JAVA/Challenge 2021/Jan/E1662_CheckIfTwoStringArraysareEquivalent.java
@@ -1,15 +1,15 @@
-import java.util.HashSet;
-
 public class E1662_CheckIfTwoStringArraysareEquivalent {
     /*
-     * Example 1: Input: word1 = ["ab", "c"], word2 = ["a", "bc"] Output: true
+     * Example 1: Input: word1 = ["ab", "c"], word2 = ["a", "bc"] 
+     * Output: true
      * Explanation: word1 represents string "ab" + "c" -> "abc" word2 represents
      * string "a" + "bc" -> "abc" The strings are the same, so return true.
      * 
-     * Example 2: Input: word1 = ["a", "cb"], word2 = ["ab", "c"] Output: false
+     * Example 2: Input: word1 = ["a", "cb"], word2 = ["ab", "c"] 
+     * Output: false
      * 
-     * Example 3: Input: word1 = ["abc", "d", "defg"], word2 = ["abcddefg"] Output:
-     * true
+     * Example 3: Input: word1 = ["abc", "d", "defg"], word2 = ["abcddefg"] 
+     * Output: true
      */
 
     class Solution {
