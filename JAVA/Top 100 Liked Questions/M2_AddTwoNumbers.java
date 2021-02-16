dd2a666f4fd9d57af4a8676eb94bd58170c38138
Jeehwan Park <yaho2k@gmail.com>
2021-02-16 18:47:12 +0900

    Add solution

---
+++ JAVA/Top 100 Liked Questions/M2_AddTwoNumbers.java
@@ -0,0 +1,55 @@
+public class M2_AddTwoNumbers {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            int sum = 0;
+            int carry = 0;
+
+            sum = l1.val + l2.val;
+            if (sum >= 10) {
+                carry = 1;
+            }
+
+            l1 = l1.next;
+            l2 = l2.next;
+
+            ListNode HeadResult = new ListNode(sum % 10);
+
+            ListNode res = HeadResult;
+            while (l1 != null || l2 != null || carry > 0) {
+
+                sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+                res.next = new ListNode(sum % 10);
+                if (sum >= 10) {
+                    carry = 1;
+                } else {
+                    carry = 0;
+                }
+                res = res.next;
+                l1 = l1 != null ? l1.next : null;
+                l2 = l2 != null ? l2.next : null;
+
+            }
+
+            return HeadResult;
+        }
+    }
+
+}
---
+++ JAVA/Top 100 Liked Questions/M3_LongestSubstringWithoutRepeatingCharacters1.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class M3_LongestSubstringWithoutRepeatingCharacters1 {
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            int result = 0;
+            int first = 0, second = 0;
+
+            HashSet<Character> hashset = new HashSet<>();
+
+            while (first < s.length()) {
+
+                if (!hashset.contains(s.charAt(first))) {
+                    hashset.add(s.charAt(first));
+                    first++;
+
+                    if (result < hashset.size()) {
+                        result = hashset.size();
+                    }
+                } else {
+                    hashset.remove(s.charAt(second));
+                    second++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
